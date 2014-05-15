2520845d4ecb007ae41678218857d361ac24dc51
Tony Wang <wwwjfy@gmail.com>
2014-05-15 16:30:49 +0800

    update packages

--- elpa/autopair-0.3/autopair-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; autopair-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("autopair.el") (21000 33448 706474 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; autopair-autoloads.el ends here
--- elpa/markdown-mode-2.0/markdown-mode-autoloads.el
+++
@@ -1,34 +0,0 @@
-;;; markdown-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (gfm-mode markdown-mode) "markdown-mode" "markdown-mode.el"
-;;;;;;  (21363 1629 0 0))
-;;; Generated autoloads from markdown-mode.el
-
-(autoload 'markdown-mode "markdown-mode" "\
-Major mode for editing Markdown files.
-
-\(fn)" t nil)
-
-(autoload 'gfm-mode "markdown-mode" "\
-Major mode for editing GitHub Flavored Markdown files.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("markdown-mode-pkg.el") (21363 1629 303689
-;;;;;;  0))
-
-;;;***
-
-(provide 'markdown-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; markdown-mode-autoloads.el ends here
