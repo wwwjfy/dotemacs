ae745cf6582532feab5fdf509ec45d29189b60d5
Tony Wang <wwwjfy@gmail.com>
2015-01-20 09:07:55 +0800

    update packages

--- elpa/fish-mode-20141026.1934/fish-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "fish-mode" "20141026.1934" "Major mode for fish shell scripts" '((emacs "24")) :keywords '("fish" "shell"))
--- elpa/goto-last-change-20121115.1014/goto-last-change-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "goto-last-change" "20121115.1014" "Move point through buffer-undo-list positions" 'nil :keywords '("convenience"))
