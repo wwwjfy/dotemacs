1dd6eddbb509821aa4d992afc8630202a4fd4073
Tony Wang <wwwjfy@gmail.com>
2018-01-15 12:05:14 +0800

    update packages

---
+++ elpa/fish-mode-0.1.3/fish-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "fish-mode" "0.1.3" "Major mode for fish shell scripts" '((emacs "24")) :commit "cd0387f7f1d5c50e080091f86ca97d8a67d603a6" :keywords '("fish" "shell"))
